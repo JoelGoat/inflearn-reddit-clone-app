87ed00af1dad2d34e9ad8e6c88b8ff813d61287a
Taekyeom Lee <tkyeom96@gmail.com>
2023-03-21 01:39:59 +0900

    이미지 업로드 라우터 생성하기(1)

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express'
+import { Router, Request, Response, NextFunction } from 'express'
 import userMiddleware from '../middlewares/user'
 import authMiddleware from '../middlewares/auth'
 import { isEmpty } from 'class-validator'
@@ -6,6 +6,9 @@ import { AppDataSource } from '../data-source'
 import Sub from '../entities/Sub'
 import User from '../entities/User'
 import Post from '../entities/Post'
+import multer, { FileFilterCallback } from 'multer'
+import { makeId } from '../utils/helpers'
+import path from 'path'
 
 const getSub = async (req: Request, res: Response) => {
   const name = req.params.name
@@ -77,10 +80,46 @@ const topSubs = async (req: Request, res: Response) => {
   }
 }
 
+const ownSub = async (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user
+
+  try {
+    const sub = await Sub.findOneOrFail({ where: { name: req.params.name } })
+
+    if (sub.username !== user.username) {
+      return res.status(403).json({ error: '이 커뮤니티를 소유하고 있지 않습니다.' })
+    }
+
+    res.locals.sub = sub
+    next()
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error: ' 문제가 발생했습니다.' })
+  }
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: 'public/images',
+    filename: (_, file, callback) => {
+      const name = makeId(10)
+      callback(null, name + path.extname(file.originalname))
+    },
+  }),
+  fileFilter: (_, file: any, callback: FileFilterCallback) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      callback(null, true)
+    } else {
+      callback(new Error('이미지가 아닙니다.'))
+    }
+  },
+})
+
 const router = Router()
 
 router.get('/:name', userMiddleware, getSub)
 router.post('/', userMiddleware, authMiddleware, createSub)
 router.get('/sub/topSubs', topSubs)
+router.post('/:name/upload', userMiddleware, authMiddleware, ownSub, upload.single('file'), uploadSubImage)
 
 export default router
