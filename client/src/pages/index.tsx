aa65707f03d4fefeb1d62a83694d8abfc139a4bc
Taekyeom Lee <tkyeom96@gmail.com>
2023-03-20 22:56:56 +0900

    커뮤니티 리스트 UI 생성

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,19 @@
 import axios from 'axios'
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 import useSWR from 'swr'
 import { Sub } from '../types'
+import { useAuthState } from '../context/auth'
 
 export default function Home() {
+  const { authenticated } = useAuthState()
+
   const fetcher = async (url: string) => {
     return await axios.get(url).then((res) => res.data)
   }
 
-  const address = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/subs/sub/topSubs`
+  const address = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/subs/sub/topSubs`
   const { data: topSubs } = useSWR<Sub[]>(address, fetcher)
 
   return (
@@ -32,12 +36,32 @@ export default function Home() {
             </div>
 
             {/* 커뮤니티 리스트 */}
-            <div></div>
-            <div className="w-full py-6 text-center">
-              <Link href="/subs/create" className="w-full p-2 text-center text-white bg-gray-400 rounded">
-                커뮤니티 만들기
-              </Link>
+            <div>
+              {topSubs?.map((sub) => (
+                <div key={sub.name} className="flex items-center px-4 py-2 text-xs border-b">
+                  <Link href={`/r/${sub.name}`}>
+                    <Image
+                      src={sub.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </Link>
+                  <Link href={`/r/${sub.name}`} className="ml-2 font-bold hover:cursor-pointer">
+                    /r/{sub.name}
+                  </Link>
+                  <p className="ml-auto font-md">{sub.postCount}</p>
+                </div>
+              ))}
             </div>
+            {authenticated && (
+              <div className="w-full py-6 text-center">
+                <Link href="/subs/create" className="w-full p-2 text-center text-white bg-gray-400 rounded">
+                  커뮤니티 만들기
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       </div>
