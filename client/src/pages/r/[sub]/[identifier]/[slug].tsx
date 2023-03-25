abfcada7831e24a0cacc2de28e638de57913d6e3
Taekyeom Lee <tkyeom96@gmail.com>
2023-03-25 23:50:24 +0900

    포스트와 댓글을 위한 Vote (좋아요 싫어요) 기능 구현하기

--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import { useAuthState } from '@/src/context/auth'
 import { FormEvent, useState } from 'react'
 import axios from 'axios'
+import { FaArrowDown, FaArrowUp } from 'react-icons/fa'
 
 const PostPage = () => {
   const router = useRouter()
@@ -13,7 +14,7 @@ const PostPage = () => {
   const { authenticated, user } = useAuthState()
   const [newComment, setNewComment] = useState('')
 
-  const { data: post } = useSWR<Post>(identifier && slug ? `/posts/${identifier}/${slug}` : null)
+  const { data: post, mutate: postMutate } = useSWR<Post>(identifier && slug ? `/posts/${identifier}/${slug}` : null)
   const { data: comments, mutate: commentMutate } = useSWR<Comment[]>(
     identifier && slug ? `/posts/${identifier}/${slug}/comments` : null,
   )
@@ -28,7 +29,30 @@ const PostPage = () => {
       await axios.post(`/posts/${post?.identifier}/${post?.slug}/comments`, {
         body: newComment,
       })
+      commentMutate()
       setNewComment('')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const vote = async (value: number, comment?: Comment) => {
+    if (!authenticated) router.push('/login')
+
+    // 이미 클릭 한 vote 버튼을 눌렀을 시에는 reset
+    if ((!comment && value === post?.userVote) || (comment && comment.userVote === value)) {
+      value = 0
+    }
+
+    try {
+      await axios.post('/votes', {
+        identifier,
+        slug,
+        commentIdentifier: comment?.identifier,
+        value,
+      })
+      postMutate()
+      commentMutate()
     } catch (error) {
       console.log(error)
     }
@@ -41,6 +65,22 @@ const PostPage = () => {
           {post && (
             <>
               <div className="flex">
+                {/* 좋아요 싫어요 기능 부분 */}
+                <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                  {/* 좋아요 */}
+                  <div
+                    className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                    onClick={() => vote(1)}>
+                    {post.userVote === 1 ? <FaArrowUp className="text-red-500" /> : <FaArrowUp />}
+                  </div>
+                  <p className="text-xs font-bold">{post.voteScore}</p>
+                  {/* 싫어요 */}
+                  <div
+                    className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                    onClick={() => vote(-1)}>
+                    {post.userVote === -1 ? <FaArrowDown className="text-blue-500" /> : <FaArrowDown />}
+                  </div>
+                </div>
                 <div className="py-2 pr-2">
                   <div className="flex items-center">
                     <p className="text-xs test-gray-400">
@@ -102,6 +142,22 @@ const PostPage = () => {
               {/* 댓글 리스트 부분 */}
               {comments?.map((comment) => (
                 <div className="flex" key={comment.identifier}>
+                  {/* 좋아요 싫어요 기능 부분 */}
+                  <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                    {/* 좋아요 */}
+                    <div
+                      className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                      onClick={() => vote(1, comment)}>
+                      {comment.userVote === 1 ? <FaArrowUp className="text-red-500" /> : <FaArrowUp />}
+                    </div>
+                    <p className="text-xs font-bold">{comment.voteScore}</p>
+                    {/* 싫어요 */}
+                    <div
+                      className="flex justify-center w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                      onClick={() => vote(-1, comment)}>
+                      {comment.userVote === -1 ? <FaArrowDown className="text-red-500" /> : <FaArrowDown />}
+                    </div>
+                  </div>
                   <div className="py-2 pr-2">
                     <p className="mb-1 text-xs leading-none">
                       <Link href={`/u/${comment.username}`} className="mr-1 font-bold hover:underline">
